33e72d5e90878bc34346fb31ccd510872eebdb70
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-08-12 12:44:44 +0800

    Add employer showcase configuration

---
+++ src/platform/showcase.ts
@@ -0,0 +1,8 @@
+export const employerShowcase = {
+  email: "employer@tindahan.test",
+  password: "tindahan123",
+  ownerName: "Maria Santos",
+  staffName: "Jun Reyes",
+  staffEmail: "staff@tindahan.test",
+  storeName: "Maria's Mini Mart",
+} as const;
